c6e0f23d940ad50336be9a0ff2bf60c6a3b51223
Zheng-Hongyi <xiaodaier@yeah.net>
2013-08-19 13:34:29 +0800

    first

---
+++ FirstStep/FirstStep/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  FirstStepAppController.h
+//  FirstStep
+//
+//  Created by zheng on 13-8-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ FirstStep/FirstStep/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstStepAppController.h
+//  FirstStep
+//
+//  Created by zheng on 13-8-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
